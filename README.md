abd1ea493bf0bd54634e033c9b3c79bfd7d635f6
fermanda <hnfmnd@gmail.com>
2017-03-12 08:50:52 +0700

    added a little help

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Native JavaScript Search Feature
 ==============
 
 This is a search feature using native JavaScript (no Library used).
-The JS will acquire data from JSON file and process it.
+The JS will acquire data from JSON file using AJAX and process it.
 
 This page is under development.
+But you can try this on (http://fermanda.github.io/njs-search)
--- jees-dev.js
+++ jees-dev.js
@@ -1,3 +1,6 @@
+//  /* */ <-- This is comment Tag
+
+//Initialization
 var i, tabs, tabcontent;
 var fetchorder = new XMLHttpRequest();
 var lastread;
@@ -5,6 +8,8 @@ tabs = document.getElementsByClassName("tab");
 
 document.getElementById("home").style.display = "block";
 
+
+//This is function for switiching from Home page to Search page
 function menu(content){
 	if(content == "orderlist") lastread = "!!@all";
 	tabcontent = document.getElementsByClassName("content");
@@ -15,19 +20,22 @@ function menu(content){
 	document.getElementById(content).style.display = "block";
 }
 
-setInterval(search, 100);
+setInterval(search, 100); //This line is used to excute search function every 100ms, so any change in search box will automatically updated
 
+
+//This is seach function
 function search(){
 	var purchase = document.getElementById("orderlist");	
 	if(purchase.style.display == "block"){
 		var find = document.getElementById("po").value;
 		if( lastread != find){
+			//This is AJAX. AJAX is used for acquiring file / data that will allow the webpage to update behind the scene
 			fetchorder.onreadystatechange=function(){
 				if (this.readyState == 4 && this.status == 200){
-					dataDisplay(this.responseText, find);
+					dataDisplay(this.responseText, find); //This is callig dataDisplay function passing AJAX file and search keywords
 				}
 			}
-			var fetchurl = "200_data.json";
+			var fetchurl = "200_data.json"; //this is what AJAX trying to acquire
 	
 			fetchorder.open("GET", fetchurl, true);
 			fetchorder.send();
@@ -36,8 +44,10 @@ function search(){
 	}	
 }
 
+//This is display function on search. Anything you search will be displayed by this function
 function dataDisplay(response, finddata){
-	var getData = JSON.parse(response);
+	var getData = JSON.parse(response); //The AJAX file will parse the file as JSON. This is also can be used for SQL. The data will be saved as array of object with properties.
+	// If the search keyword is blank, this condition will show all data
 	if(finddata.length == 0){
 		var dataShow = '<table class=purchasedata width="100%">';
 		dataShow += "<tr>";
@@ -57,10 +67,13 @@ function dataDisplay(response, finddata){
 		}
 		
 		dataShow += "</table>";
-		document.getElementById("data").innerHTML = dataShow;
+		document.getElementById("data").innerHTML = dataShow; //This line will search in HTML file, element with id="data", and fill it with dataShow content
 	}
+
+	// If there's some keyword in search (Or alphabet containing), this condition will show only the data needed / searched
 	else{
 		var findresult = [];
+		// This for function here is searching in array of object that have properties containing the keyword and save it in a new array of objects
 		for(i = 0; i < getData.length; i++){
 			for(key in getData[i]){
 				if(getData[i][key].toString().toUpperCase().indexOf(finddata.toUpperCase()) != -1) {
@@ -69,6 +82,7 @@ function dataDisplay(response, finddata){
 				}
 			}
 		}
+
 		var dataShow = '<table class=purchasedata width="100%">';
 		dataShow += "<tr>";
 		dataShow += '<th width="5%">NO.</th>';
